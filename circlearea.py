f3ea16ea7e017a1f513c8552e94a4d93ad8ccdb7
JEEVAABI <jeevaabishake@gmail.com>
2022-05-12 14:51:04 +0530

    AIBASEDAPP

--- circlearea.py
+++ circlearea.py
@@ -2,6 +2,6 @@ import math
 print("AREA OF CIRCLE")
 print("Formula is pi*radius**2")
 radius = int(input("Enter radius of circle: "))
-total=(math.pi*radius**25)
+total=(math.pi*radius**2)
  
 print("Area of circle is : ", total)
