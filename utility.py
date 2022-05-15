731c4bcc6ea521b6bd29c42bc56e70f750d8eaaa
Sugan2002 <suganya.parthiban2002@gmail.com>
2022-05-15 09:21:26 +0000

    Sugan

--- utility.py
+++ utility.py
@@ -8,3 +8,19 @@ else:
    for i in range(1,num + 1):    
        factorial = factorial*i    
    print("The factorial of",num,"is",factorial)   
+
+
+
+
+import math  
+def compound_interest(p,r,t):   
+    amt = p * (math.pow((1 + (r/100)),t))
+    print("Compound Amount: ",amt)
+    CI = amt - p
+    return CI
+
+p=float(input("Enter Principal Amount: "))
+r=float(input("Enter Rate of Interest: "))
+t=float(input("Enter Time Period in years: "))
+
+print("Compound interest is",compound_interest(p,r,t))
